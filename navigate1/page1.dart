e1f0b6123cf4f386dfd7281764231e83c03fad85
Shiva koti <167282319+parthugari-shivakoti@users.noreply.github.com>
2025-05-07 11:13:31 +0530

    Create page1.dart

---
+++ navigate1/page1.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'page2.dart'; // Import the file for Page2
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'This is Page 1.',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Page2()),
+                );
+              },
+              child: const Text('Go to Page 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
